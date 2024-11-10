afd5ede8773df3a0443ff4865ef6aa0acd114e93
Abdallah-Basem-Zain <120277643+Abdallah-Basem-Zain@users.noreply.github.com>
2024-11-10 13:30:08 +0200

    1071 Greatest Common Divisor

---
+++ src/easy/GreatestCommonDivisor_1071/Solution.java
@@ -0,0 +1,23 @@
+package easy.GreatestCommonDivisor_1071;
+
+public class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+    }
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().gcdOfStrings("EEEE", "EE"));
+    }
+}
